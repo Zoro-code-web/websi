0e067b371306730fc9384acec66a1b66153f9829
Zoro-pro-code <virtuegreat3@gmail.com>
2024-10-06 00:17:32 +0100

    initial vibes

---
+++ perfection/mine.js
@@ -0,0 +1,34 @@
+
+function demofunction(){
+    alert("do you like this page")
+    var check;
+    check=prompt("are u sure")
+    var check;
+    if(check=="no"){
+        var b="fuck  you";
+        alert(b);
+    }
+    if(check=="yes"){
+        var a="you are welcome";
+        alert(a);
+    }
+    else{
+        alert("you have canceled this operation")
+    }
+}
+// document.getElementById('show-login').addEventListener
+// ('click', function(event){
+//     event.preventDefault();
+//     document.getElementById('sign-up').classList;
+//     document.getElementById('login-form').classList.add('active');
+// })
+
+
+
+// document.getElementById('').addEventListener
+// ('click', function(event){
+//     event.preventDefault();
+//     document.getElementById('sign-form').classList.
+//     remove('post');
+//     document.getElementById('').classList.add('post');
+// })
